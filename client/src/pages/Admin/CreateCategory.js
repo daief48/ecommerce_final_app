309998bf5464839ee5fbfd3eb3a08c5b9c751a79
daief48 <daief20103048@gmail.com>
2023-07-17 17:04:05 +0600

    add product forntend

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -35,8 +35,8 @@ const CreateCategory = () => {
     const getAllCategory = async () => {
         try {
             const { data } = await axios.get("http://localhost:8080/api/v1/category/get-category");
-            if (data.success) {
-                setCategories(data.category);
+            if (data?.success) {
+                setCategories(data?.category);
                 getAllCategory();
             }
         } catch (error) {
