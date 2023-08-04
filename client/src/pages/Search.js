c6d104232fc4512af5c8513849a731edcd7aecad
daief48 <daief20103048@gmail.com>
2023-08-05 00:21:18 +0600

    modify search functionality

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -15,7 +15,7 @@ const Search = () => {
             {values?.results.map((p) => (
               <div className="card m-2" style={{ width: "18rem" }}>
                 <img
-                  src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                  src={`https://ecommerce-final-app-backend.onrender.com/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name} 
                   style={{height:"300px"}}
